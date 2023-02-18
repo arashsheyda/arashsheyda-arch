d6679c6a689362cc3ebd04f093356fba1e7f198e
arashsheyda <sheidaeearash1999@gmail.com>
2023-02-18 19:50:12 +0300

    arch v-0.0.0

---
+++ app.config.ts
@@ -0,0 +1,52 @@
+export default defineAppConfig({
+  arch: {
+    title: 'Arash Sheyda',
+    description: 'Arash Sheyda - Full Stack Developer',
+    keywords: 'Arash Sheyda, Full Stack Developer',
+    banner: {
+      image: '/public/assets/images/banner.jpg'
+    },
+    info: {
+      image: '/public/assets/images/logo.jpg',
+      hoverImage: '/public/assets/images/logo.jpg',
+      name: 'Arash Sheyda',
+      job: 'Full Stack Developer',
+      email: 'info@arashshyeda.com',
+      phone: '+1 (732) 807-5358',
+      company: '',
+      location: 'Alberta, CA',
+    },
+    footer: {
+      copyright: 'Arash Sheyda',
+      links: [
+        {
+          title: '🎯 Goals',
+          url: '/goals'
+        }
+      ]
+    },
+    socials: [
+      {
+        name: 'Github',
+        icon: 'uil:github',
+        url: 'https://github.com/arashsheyda',
+        color: '#000000',
+        target: '_blank'
+      },
+      {
+        name: 'Instgaram',
+        icon: 'uil:instagram',
+        url: 'https://www.instagram.com/arash.sheyda',
+        color: '#e4405f',
+        target: '_blank'
+      },
+      {
+        name: 'Twitter',
+        icon: 'uil:twitter',
+        url: 'https://twitter.com/arash_sheyda',
+        color: '#1da1f2',
+        target: '_blank'
+      }
+    ],
+  }
+})
